d0af3bb15dd8f591010f3e983021eac626a4fd9b
Oleg Dreyman <dreymonde@me.com>
2018-02-11 18:55:33 +0200

    Update script

--- Sources/CorbusierLang/stdlib.swift
+++ Sources/CorbusierLang/stdlib.swift
@@ -2,8 +2,4 @@ internal let stdlib = """
 def subtract(a, b) {
 	return add(a, negate(b))
 }
-
-def zero() {
-	return 0
-}
 """
--- generate-stdlib.sh
+++ generate-stdlib.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 rm -f Sources/CorbusierLang/stdlib.swift
 
@@ -6,3 +6,7 @@ touch Sources/CorbusierLang/stdlib.swift
 echo $'internal let stdlib = """' >> Sources/CorbusierLang/stdlib.swift
 cat stdlib/stdlib.sier >> Sources/CorbusierLang/stdlib.swift
 echo $'"""' >> Sources/CorbusierLang/stdlib.swift
+
+echo "New stdlib.swift file generated in Sources/CorbusierLang/stdlib.swift:"
+echo ""
+cat Sources/CorbusierLang/stdlib.swift
